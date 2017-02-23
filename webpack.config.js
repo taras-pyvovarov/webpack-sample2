c0cfdd74c2dfc4c11423280070d20026b6b248a7
Taras <taras.pyvovarov@gmail.com>
2017-02-23 19:39:50 +0200

    More complex bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
 const webpack = require('webpack');
+
 module.exports = {
   context: path.resolve(__dirname, './src'),
   entry: {
-    app: './app.js',
+    //Bundle with key 'app'
+    app: ['./home.js', './app.js'],
+
+    //Bundle with key 'vendor'
+    vendor: './vendor.js',
+
+    //Bundle with key 'contact'
+    contact: './contact.js',
   },
   output: {
     path: path.resolve(__dirname, './dist'),
