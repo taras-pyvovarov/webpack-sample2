eaa417bc6993859d7824b7dc4811c9f875b31b73
Taras <taras.pyvovarov@gmail.com>
2017-02-24 16:59:14 +0200

    Added webpack dev server support

--- webpack.config.js
+++ webpack.config.js
@@ -19,15 +19,23 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, './dist'),
+    //Path where outputs are placed
+    path: path.resolve(__dirname, './dist/assets'),
     filename: '[name].bundle.js',
+    //Path in webpack web server where file will be available
+    publicPath: '/assets',
+  },
+
+  devServer: {
+    //This 'src' folder will be served in devserver to be available in localhost root
+    contentBase: path.resolve(__dirname, './src'),
   },
 
   plugins: [
     //Describe vendor bundle
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: 'vendor.bundle.js'
-    }),
+            name: 'vendor',
+            filename: 'vendor.bundle.js'
+        }),
   ],
 };
